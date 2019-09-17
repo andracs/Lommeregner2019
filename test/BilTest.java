1ec79e68dbcf81c8ecf3f111056cdcaf83234c6f
András Ács <acsandras@gmail.com>
2019-09-17 11:00:39 +0200

    Har tilføjet flere tests.

---
+++ test/BilTest.java
@@ -0,0 +1,17 @@
+import junit.framework.TestCase;
+
+public class BilTest extends TestCase {
+
+    public void testStart() {
+        
+    }
+
+    public void testStop() {
+    }
+
+    public void testGearUp() {
+    }
+
+    public void testGearNed() {
+    }
+}
